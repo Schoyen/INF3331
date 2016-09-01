f88e4a0bb308ef98b0b25a10f2e0fc1c89808ea3
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-09-01 17:04:48 +0200

    Successfully retrieved the departure time
    * Currently only the first subway will be outputted

--- assignment2/subway.sh
+++ assignment2/subway.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+#ruter_data=$(curl --request GET "https://reisapi.ruter.no/place/getstopsruter")
+ruter_data=$(<test.json)
+ID_blindern=$(echo $ruter_data | grep -E -so '.{0,16}Blindern \[T-bane\].' | cut -c -7)
+#blindern_data=$(curl --request GET "https://reisapi.ruter.no/stopvisit/getdepartures/$ID_blindern")
+blindern_data=$(<blindern.json)
+blindern_departure_time=${blindern_data#*ExpectedDepartureTime}
+blindern_departure_time=${blindern_departure_time#*T}
+blindern_departure_time=${blindern_departure_time%%+*}
+echo $blindern_departure_time
