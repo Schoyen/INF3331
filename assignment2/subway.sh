ad78d48d3f71a113143021f26379c08d76f00f5f
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-09-01 19:03:34 +0200

    Attempting to use substring removal
    * As an alternative to grep and cut

--- assignment2/subway.sh
+++ assignment2/subway.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 #ruter_data=$(curl --request GET "https://reisapi.ruter.no/place/getstopsruter")
-ruter_data=$(<test.json)
-ID_blindern=$(echo $ruter_data | grep -E -so '.{0,16}Blindern \[T-bane\].' | cut -c -7)
-#blindern_data=$(curl --request GET "https://reisapi.ruter.no/stopvisit/getdepartures/$ID_blindern")
-blindern_data=$(<blindern.json)
-blindern_departure_time=${blindern_data#*ExpectedDepartureTime}
-blindern_departure_time=${blindern_departure_time#*T}
-blindern_departure_time=${blindern_departure_time%%+*}
-echo $blindern_departure_time
+ruter_stops=$(<test.json)
+
+id_stations=${ruter_stops%%Forskningsparken \[T-bane\]*}
+echo $id_stations
+
+#if [ $# -eq 0 ]; then
+#    ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Forskningsparken \[T-bane\].' | cut -c -7)
+#fi
+#
+#if [ "$1" == "Blindern" ]; then
+#    ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Blindern \[T-bane\].' | cut -c -7)
+#elif [ "$1" == "Ullevål stadion" ]; then
+#    ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Ullevål stadion \[T-bane\].' | cut -c -7)
+#fi
+
+#blindern_data=$(curl --request GET "https://reisapi.ruter.no/stopvisit/getdepartures/$ID_station" > stations.json)
+#blindern_data=$(<stations.json)
+#blindern_departure_time=${blindern_data#*ExpectedDepartureTime}
+#blindern_departure_time=${blindern_departure_time#*T}
+#blindern_departure_time=${blindern_departure_time%%+*}
+#echo $blindern_departure_time
