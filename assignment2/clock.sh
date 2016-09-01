eddd76fe6d06c2e48eaa96d65f7282e2ede7dbd6
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-09-01 11:06:54 +0200

    Working and commented clock.sh

--- assignment2/clock.sh
+++ assignment2/clock.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+while [ "true" ] # Run until CTRL-c is encountered
+do
+    clear # Clear terminal
+
+    if [ "$1" == "--AMPM" ]; then # Check if additional argument is "--AMPM"
+        date=$(date +"%r") # If so, get the time as a 12-hour clock
+    else
+        date=$(date +"%T") # If not, get time as a 24-hour clock
+    fi
+
+    echo $date # Output the time
+    sleep 1 # Sleep for one second, before repeating
+done
