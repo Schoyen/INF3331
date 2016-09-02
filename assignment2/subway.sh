baa219a70f82374b0748a717a0984e1ab5efa373
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-09-02 14:46:16 +0200

    Working ID-testing and ID-retrieval for all

--- assignment2/subway.sh
+++ assignment2/subway.sh
@@ -3,20 +3,22 @@
 #ruter_stops=$(curl --request GET "https://reisapi.ruter.no/place/getstopsruter" > test.json)
 ruter_stops=$(<test.json)
 
-id_stations=${ruter_stops%,\"Name\":\"Forskningsparken \[T\-bane\]*}
-id_stations=${id_stations##*\"ID\":}
+
+if [ $# -eq 0 ]; then
+    id_stations=${ruter_stops%,\"Name\":\"Forskningsparken \[T\-bane\]*}
+    id_stations=${id_stations##*\"ID\":}
+    #ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Forskningsparken \[T-bane\].' | cut -c -7)
+elif [ "$1" == "Blindern" ]; then
+    id_stations=${ruter_stops%,\"Name\":\"Blindern \[T\-bane\]*}
+    id_stations=${id_stations##*\"ID\":}
+    #ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Blindern \[T-bane\].' | cut -c -7)
+elif [ "$1" == "Ullevål stadion" ]; then
+    id_stations=${ruter_stops%,\"Name\":\"Ullevål stadion \[T\-bane\]*}
+    id_stations=${id_stations##*\"ID\":}
+    #ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Ullevål stadion \[T-bane\].' | cut -c -7)
+fi
+
 echo $id_stations
-
-#if [ $# -eq 0 ]; then
-#    ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Forskningsparken \[T-bane\].' | cut -c -7)
-#fi
-#
-#if [ "$1" == "Blindern" ]; then
-#    ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Blindern \[T-bane\].' | cut -c -7)
-#elif [ "$1" == "Ullevål stadion" ]; then
-#    ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Ullevål stadion \[T-bane\].' | cut -c -7)
-#fi
-
 #blindern_data=$(curl --request GET "https://reisapi.ruter.no/stopvisit/getdepartures/$ID_station" > stations.json)
 #blindern_data=$(<stations.json)
 #blindern_departure_time=${blindern_data#*ExpectedDepartureTime}
