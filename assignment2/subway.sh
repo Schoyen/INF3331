ecef5b6c8e20e771a3f17f95bad2bc9657a6d503
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-09-02 15:04:20 +0200

    Started on support for command line arguments

--- assignment2/subway.sh
+++ assignment2/subway.sh
@@ -8,7 +8,10 @@ if [ $# -eq 0 ]; then
     id_stations=${ruter_stops%,\"Name\":\"Forskningsparken \[T\-bane\]*}
     id_stations=${id_stations##*\"ID\":}
     #ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Forskningsparken \[T-bane\].' | cut -c -7)
-elif [ "$1" == "Blindern" ]; then
+fi
+
+
+if [ "$1" == "Blindern" ]; then
     id_stations=${ruter_stops%,\"Name\":\"Blindern \[T\-bane\]*}
     id_stations=${id_stations##*\"ID\":}
     #ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Blindern \[T-bane\].' | cut -c -7)
@@ -18,10 +21,10 @@ elif [ "$1" == "Ullevål stadion" ]; then
     #ID_station=$(echo $ruter_data | grep -E -so '.{0,16}Ullevål stadion \[T-bane\].' | cut -c -7)
 fi
 
-echo $id_stations
-#blindern_data=$(curl --request GET "https://reisapi.ruter.no/stopvisit/getdepartures/$ID_station" > stations.json)
-#blindern_data=$(<stations.json)
-#blindern_departure_time=${blindern_data#*ExpectedDepartureTime}
+#echo $id_stations
+#station_data=$(curl --request GET "https://reisapi.ruter.no/stopvisit/getdepartures/$id_stations" > stations.json)
+station_data=$(<stations.json)
+departure_time=${blindern_data#*ExpectedDepartureTime}
 #blindern_departure_time=${blindern_departure_time#*T}
 #blindern_departure_time=${blindern_departure_time%%+*}
 #echo $blindern_departure_time
