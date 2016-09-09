4e93f68b08b1818e0da6a9a2513090efd5a2a91e
Schoyen <oyvindschoyen@gmail.com>
2016-09-09 13:32:27 +0200

    Working unit test
    * Uncommented

--- assignment3/my_unit_testing.py
+++ assignment3/my_unit_testing.py
@@ -1,8 +1,18 @@
 class UnitTest(object):
     def __init__(self, func, args, kwargs, res):    # make test
-        NotImplemented          
+
+        if callable(func):
+            self.func = func
+        else:
+            print ("[DEBUG] In %s UnitTest.__init__: Parameter func is not callable. Treating as a callable constant" % __name__)
+            self.func = lambda a, b, num_rechecks=2: func # TODO: Check if number of arguments in lambda can be arbitrary
+
+        self.args = args
+        self.kwargs = kwargs
+        self.res = res
 
     def __call__(self):                             # run test
-        NotImplemented
-
-
+        try:
+            return self.func(self.args[0], self.args[1], num_rechecks=self.kwargs["num_rechecks"]) == self.res
+        except IndexError:
+            return False
