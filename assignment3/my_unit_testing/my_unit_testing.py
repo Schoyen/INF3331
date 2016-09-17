cf66741ab15b93bdf7257a3a139d00141353b3f7
Schoyen <oyvindschoyen@gmail.com>
2016-09-17 18:26:09 +0200

    Remove tests in my_unit_testing.py

--- assignment3/my_unit_testing/my_unit_testing.py
+++ assignment3/my_unit_testing/my_unit_testing.py
@@ -75,28 +75,3 @@ if __name__ == '__main__':
     print (UnitTest.__init__.__doc__)
     print (UnitTest.__call__.__doc__)
     print (UnitTest.tolerance.__doc__)
-
-    # Short and simple testing of the module
-    func = lambda a, b, c: a + b + c
-    func_const = 6
-    func_no_param = lambda: 0
-    func_no_args = lambda a=2: a
-    func_all = lambda a, b, c=3: a * b + c
-    func_random = lambda a=1, b=2, c=3: a + b + c
-    a = 1
-    b = 2
-    c = 3
-    res = 6
-    unit = UnitTest(func, [a, b, c], None, res)
-    assert unit(), "[ERROR] %g != %d" % (func(a, b, c), res)
-    unit = UnitTest(func_const, [a, b, c], None, res)
-    assert unit(), "[ERROR] %g != %d" % (func_const, res)
-    unit = UnitTest(func_no_param, None, None, 0)
-    assert unit(), "[ERROR] %g != %d" % (func_no_param(), 0)
-    unit = UnitTest(func_no_args, None, {'a': 3}, 3)
-    assert unit(), "[ERROR] %g != %d" % (func_no_args(3), 3)
-    unit = UnitTest(func_all, [1, 2], {'c': 3}, 5)
-    assert unit(), "[ERROR] %g != %d" % (func_all(1, 2, 3), 5)
-
-    unit = UnitTest(func_random, None, {'c': 5, 'b': 2, 'a': 1}, 8)
-    assert unit(), "[ERROR] %g != %d" % (func_random(a=1, b=2, c=5), 8)
