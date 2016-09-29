4f70fa286fb043bc63441592e90728d5300c91d9
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-09-29 14:05:20 +0200

    Working numpy implementation of the mandelbrot set

--- assignment4/mandelbrot_2.py
+++ assignment4/mandelbrot_2.py
@@ -1,10 +1,29 @@
-from numpy import linspace, array, zeros, sqrt
+from numpy import linspace, ogrid, array, zeros, conj
+from matplotlib.pylab import imshow, show, savefig
+from tqdm import tqdm
 
-n = 1000
-threshold = 1000
-c = array([linspace(-2, 0.5, n), linspace(-1, 1, n)])
-z = zeros((n, n))
-counter = 0
 
-while counter < threshold:
-    z[sqrt(z[0]**2 + z[1]**2) <= 2] = array([z[0]**2 - z[1]**2 + c[0], 2*z[0]*z[1] + c[1]])
+class MandelbrotVec:
+
+    def __init__(self, height, width, threshold=100):
+        self.height = height
+        self.width = width
+        self.threshold = threshold
+
+    def __call__(self, divergence_criteria=2):
+        c_complex, c_real = ogrid[-1.4:1.4:self.height*1j, -2:0.8:self.width*1j]
+        c = c_real + 1j*c_complex
+        z = zeros(c.shape, dtype=complex)
+        divergence_steps = zeros(z.shape, dtype=int)
+        for i in tqdm(range(self.threshold)):
+            indices = z*conj(z) <= divergence_criteria
+            z[indices] = z[indices]**2 + c[indices]
+            divergence_steps[indices] += 1
+        return divergence_steps
+
+
+if __name__ == '__main__':
+    mv = MandelbrotVec(1000, 1000, threshold=100)
+    imshow(mv(divergence_criteria=2))
+    savefig("dat/mandelbrot_numpy.pdf")
+    show()
