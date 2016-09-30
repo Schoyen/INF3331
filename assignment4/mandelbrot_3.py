c7b83f97fcad4649ad5f2ef9530ee6e5a7cca671
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-09-30 13:39:26 +0200

    Update

--- assignment4/mandelbrot_3.py
+++ assignment4/mandelbrot_3.py
@@ -4,4 +4,4 @@ height = 10
 width = 10
 threshold = 10
 divergence_criteria = 2
-print (mandelbrot_set(height, width, threshold, divergence_criteria).shape)
+print (mandelbrot_set(height, width, threshold, divergence_criteria))
