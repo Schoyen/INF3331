8f5a2907c18892a7e204f14ae0daf7e82b0390e5
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-04 13:33:12 +0200

    Rename dir mandelbrot -> src

--- assignment4/mandelbrot-client/setup.py
+++ assignment4/mandelbrot-client/setup.py
@@ -4,8 +4,8 @@ from Cython.Distutils import build_ext
 setup(name="Mandelbrot",
       version='0.1',
       author=u'Øyvind Sigmundson Schøyen',
-      packages=['mandelbrot', 'mandelbrot.commands', 'mandelbrot.tests'],
-      ext_modules=[Extension("mandelbrot", ["mandelbrot/commands/mandelbrot.pyx"])],
+      packages=['src', 'src.commands', 'src.tests'],
+      ext_modules=[Extension("mandelbrot", ["src/commands/mandelbrot.pyx"])],
       cmdclass={'build_ext': build_ext},
       data_files=[('config', ['setup.cfg'])],
      )
