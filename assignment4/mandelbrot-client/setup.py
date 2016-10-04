7ccd6d17954e314ea9014227ef8200393f44505d
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-04 14:48:22 +0200

    Fix placement of cython .so-files in install dir

--- assignment4/mandelbrot-client/setup.py
+++ assignment4/mandelbrot-client/setup.py
@@ -1,11 +1,21 @@
-from distutils.core import setup, Extension
+from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+from os.path import abspath, dirname, join # Needed to get location of documentation
+
+with open(join(abspath(dirname(__file__)), 'README.rst'), 'r', encoding='utf-8') as f:
+    documentation = f.read()
+
+extensions=[Extension("src.commands.mandelbrot", ["src/commands/mandelbrot.pyx"])]
 
 setup(name="Mandelbrot",
       version='0.1',
+      description='Hallo',
+      long_description=documentation,
       author=u'Øyvind Sigmundson Schøyen',
       packages=['src', 'src.commands', 'src.tests'],
-      ext_modules=[Extension("mandelbrot", ["src/commands/mandelbrot.pyx"])],
+      ext_modules=extensions,
       cmdclass={'build_ext': build_ext},
       data_files=[('config', ['setup.cfg'])],
      )
