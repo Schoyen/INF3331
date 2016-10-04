5b82bc5f5fec17ed764ac05c89fcd525b01ac4bc
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-04 15:17:23 +0200

    Update setup.py to use setuptools
            This commit changes distutils to setuptools to allow for
            entry_points inside setup(). This made all the build files from
            Cython return to this directory

--- assignment4/mandelbrot-client/setup.py
+++ assignment4/mandelbrot-client/setup.py
@@ -1,8 +1,20 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Command, Extension
+#from distutils.core import setup, Command
+#from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 from os.path import abspath, dirname, join # Needed to get location of documentation
+
+class ExecuteTests(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print ("Hello, World!")
 
 with open(join(abspath(dirname(__file__)), 'README.rst'), 'r', encoding='utf-8') as f:
     documentation = f.read()
@@ -14,8 +26,11 @@ setup(name="Mandelbrot",
       description='Hallo',
       long_description=documentation,
       author=u'Øyvind Sigmundson Schøyen',
-      packages=['src', 'src.commands', 'src.tests'],
+      packages=['src', 'src.commands'],
       ext_modules=extensions,
-      cmdclass={'build_ext': build_ext},
+      entry_points={
+          'console_scripts': ['mandelbrot=src.command_client.py:main',],
+          },
+      cmdclass={'build_ext': build_ext, 'test': ExecuteTests},
       data_files=[('config', ['setup.cfg'])],
      )
