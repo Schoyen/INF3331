bd93fe3a428e7471fd472108c55bce93d3dd6006
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-04 13:07:30 +0200

    Start setup.py for root dir

---
+++ assignment4/mandelbrot-client/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name="Mandelbrot",
+      version='0.1',
+      author=u'Øyvind Sigmundson Schøyen',
+      packages=['mandelbrot', 'mandelbrot.commands', 'mandelbrot.tests'],
+     )
