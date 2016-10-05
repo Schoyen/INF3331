7f555fd283d0c7bfa776a622ad961d6820cb4554
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-05 13:38:54 +0200

    Rewrite using setuptools
            This commit includes test template using unittest

--- assignment4/mandelbrot-client/setup.py
+++ assignment4/mandelbrot-client/setup.py
@@ -1,33 +1,25 @@
-#from setuptools import setup, Command, Extension
-from distutils.core import setup, Command
-from distutils.extension import Extension
+from setuptools import setup, Extension
+#from distutils.core import setup, Command
+#from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 from os.path import abspath, dirname, join # Needed to get location of documentation
 
-class ExecuteTests(Command):
-    user_options = []
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        print ("Hello, World!")
-
-with open(join(abspath(dirname(__file__)), 'README.rst'), 'r', encoding='utf-8') as f:
-    documentation = f.read()
+def documentation():
+    with open(join(abspath(dirname(__file__)), 'README.rst'), 'r', encoding='utf-8') as f:
+        return f.read()
 
 extensions=[Extension("src.commands.mandelbrot", ["src/commands/mandelbrot.pyx"])]
 
 setup(name="Mandelbrot",
       version='0.1',
       description='Hallo',
-      long_description=documentation,
+      long_description=documentation(),
       author=u'Øyvind Sigmundson Schøyen',
       packages=['src', 'src.commands'],
       ext_modules=extensions,
-      cmdclass={'build_ext': build_ext, 'test': ExecuteTests},
+      cmdclass={'build_ext': build_ext},
       data_files=[('config', ['setup.cfg'])],
+      test_suite='nose.collector',
+      tests_require=['nose'],
      )
---
+++ assignment4/mandelbrot-client/src/tests/test_test.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+class TestTest(TestCase):
+
+    def test_is_test(self):
+        a = True
+        assert a
