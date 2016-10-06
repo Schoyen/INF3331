a2467dffae65a63459c25b383bd81590ce2b5c85
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-06 19:26:12 +0200

    Start implementation of CLI
            Switch to argparse instead of manual iteration

--- assignment4/mandelbrot/mandelbrot/mandelbrot_client.py
+++ assignment4/mandelbrot/mandelbrot/mandelbrot_client.py
@@ -1,12 +1,40 @@
-from . import silly
 from .usage import display_help
 from sys import argv
 from .src import MandelbrotPython, MandelbrotNumpy, MandelbrotCython
+from argparse import ArgumentParser
 
 def main():
-    print (u"This is main\nNow calling silly() in __init__.py...")
-    silly()
-    print ("These are your command line arguments")
-    print (argv)
-    print (display_help())
+    description = """
+    A command line interface for controlling three different Python implementations
+    computing and displaying the Mandelbrot. The three different implementations are
+    written using Python, Python with Numpy vectorization and Cython.
+    """
+    parser = ArgumentParser(description=description)
+    parser.add_argument("-c", "--cython", help="Create the Mandelbrot set using Cython")
+    parser.parse_args()
+#    mc = MandelbrotClient(argv)
+#    mc()
 
+class MandelbrotClient:
+
+    def __init__(self, args):
+        self.args = args
+        self.valid_commands = []
+        for i in display_help().split():
+            if i == "Usage:":
+                break
+            if i.startswith("-"):
+                self.valid_commands.append(i)
+        self.commands = {"help": display_help,
+                "cython": MandelbrotCython,
+                "python": MandelbrotPython,
+                "numpy": MandelbrotNumpy}
+
+    def __call__(self):
+        if len(self.args) < 2:
+            print(display_help())
+        else:
+            if self.args[1] in self.valid_commands:
+                pass
+            else:
+                print("mandelbrot: command not found: %s" % self.args[1])
--- assignment4/mandelbrot/mandelbrot/usage.py
+++ assignment4/mandelbrot/mandelbrot/usage.py
@@ -8,5 +8,14 @@ Synopsis:
     computing and displaying the Mandelbrot. The three different implementations are
     written using Python, Python with Numpy vectorization and Cython.
 
+Options:
+    -h --help                       Display this help message
+    -c --cython                     Use Cython implementation
+    -p --python                     Use Python implementation
+    -n --numpy                      Use Numpy implementation
+
 Usage:
+    mandelbrot -[cpn] xmin xmax ymin ymax Nx Ny filename
+
+Here 'filename' is optional
 """
