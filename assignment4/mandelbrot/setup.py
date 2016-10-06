a69b426cff43124299589695c0f0d26b70c12a63
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-06 13:23:04 +0200

    Update classifiers in setup.py and .gitignore

--- assignment4/mandelbrot/setup.py
+++ assignment4/mandelbrot/setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="mandelbrot",
       version='0.1',
       description='Create mandelbrot sets',
       author=u'Øyvind Sigmundson Schøyen',
-      packages=['src'],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+      ],
+      packages=find_packages(),
      )
