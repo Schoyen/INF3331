b763dda2eb8f56c289968cbb780f4c870cf1563e
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-06 17:31:04 +0200

    Update Python implementation of the Mandelbrot set

--- assignment4/mandelbrot/mandelbrot/src/mandelbrot_1.py
+++ assignment4/mandelbrot/mandelbrot/src/mandelbrot_1.py
@@ -1,67 +1,30 @@
-from math import sqrt
-from matplotlib.pylab import imshow, figure, show, axis, savefig
-from tqdm import tqdm
 
-class MandelbrotSet:
+class MandelbrotPython:
 
-    def __init__(self, c, threshold=100):
-        self.a, self.b = self.test_if_list_or_tuple(c, "c")
-        self.threshold = threshold
+    def __init__(self, xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=1000, divergence_criteria=2):
+        self.xmin = xmin; self.xmax = xmax; self.Nx = Nx
+        self.ymin = ymin; self.ymax = ymax; self.Ny = Ny
+        self.max_escape_time = max_escape_time
+        self.divergence_criteria = divergence_criteria
 
-    def __call__(self, z):
-        x, y = self.test_if_list_or_tuple(z, "z")
-        counter = 0
-        x, y = (x**2 - y**2 + self.a, 2*x*y + self.b)
-        abs_z = sqrt(x**2 + y**2)
-        while counter < self.threshold and abs_z <= 2:
-            counter += 1
-            x, y = (x**2 - y**2 + self.a, 2*x*y + self.b)
-            abs_z = sqrt(x**2 + y**2)
-        return abs_z, counter
-            
-    def test_if_list_or_tuple(self, value, name):
-        if type(value) == tuple or type(value) == list:
-            return value
-        else:
-            try:
-                return (float(value), 0) # Assuming a real number was sent
-            except ValueError:
-                raise ValueError("Variable '%s' must be a list, tuple or a real number." % (name))
+        self.mandelbrot_set = [[0 for i in range(self.Nx)] for j in range(self.Ny)]
+        self.dx = (self.xmax - self.xmin)/float(self.Nx - 1)
+        self.dy = (self.ymax - self.ymin)/float(self.Ny - 1)
 
-
-def create_mandelbrot_set():
-    x_start = -2
-    y_start = -1
-    x_stop = 0.5
-    y_stop = 1
-    n = 100
-    threshold = 1000
-    dx = (x_stop - x_start)/float(n - 1)
-    dy = (y_stop - y_start)/float(n - 1)
-
-    # Create a white image to begin with
-    A = [[[1, 1, 1] for j in range(n)] for i in range(n)]
-
-    ms = MandelbrotSet((x_start, y_start), threshold=threshold)
-    for i in tqdm(range(n)):
-        for j in range(n):
-            ms.a, ms.b = (x_start + i*dx, y_start + j*dx)
-            abs_z, counter = ms(0)
-            if abs_z <= 2:
-                A[j][i] = [0, 0, 0] # Color pixel black if it is in the mandelbrot set (i.e., abs_z <= 2)
-            else:
-                if counter < threshold/3.0:
-                    color = [counter/float(threshold), 0.7, 0.7]
-                elif counter < 2*threshold/3.0:
-                    color = [0.5, counter/float(threshold), 0.5]
-                else:
-                    color = [0.2, 0.2, counter/float(threshold)]
-                A[j][i] = color
-
-    figure(1)
-    imshow(A, extent=[x_start, x_stop, y_start, y_stop])
-    savefig("mandelbrot.png")
-    show()
+    def __call__(self):
+        for j in range(self.Ny):
+            for i in range(self.Nx):
+                x = c_real = self.xmin + i*self.dx
+                y = c_imag = self.ymin + j*self.dy
+                abs_z_squared = x*x + y*y
+                while self.mandelbrot_set[j][i] < self.max_escape_time and abs_z_squared < self.divergence_criteria**2:
+                    self.mandelbrot_set[j][i] += 1
+                    x, y = (x*x - y*y + c_real, 2*x*y + c_imag)
+                    abs_z_squared = x*x + y*y
+        return self.mandelbrot_set
 
 if __name__ == '__main__':
-    create_mandelbrot_set()
+    from matplotlib.pylab import imshow, show
+    mp = MandelbrotPython(-2, 0.5, -1, 1, 100, 200)
+    imshow(mp())
+    show()
