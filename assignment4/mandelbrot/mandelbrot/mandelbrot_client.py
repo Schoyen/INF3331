d3ab5df14ac284ecca1ec487d855ed4aa46844c8
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-06 13:48:20 +0200

    Use sys.argv to process command-line input

--- assignment4/mandelbrot/mandelbrot/mandelbrot_client.py
+++ assignment4/mandelbrot/mandelbrot/mandelbrot_client.py
@@ -1,5 +1,8 @@
 from . import silly
+from sys import argv
 
 def main():
     print (u"This is main\nNow calling silly() in __init__.py...")
     silly()
+    print ("These are your command line arguments")
+    print (argv)
