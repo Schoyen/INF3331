d9d019ca2c38ea89fea709b22da6b4d97908dc6e
Schoyen-Linux <oyvinssc@student.matnat.uio.no>
2016-10-08 13:20:55 +0200

    Make compute_mandelbrot return the escapte_matrix

--- assignment4/mandelbrot/mandelbrot/compute_mandelbrot.py
+++ assignment4/mandelbrot/mandelbrot/compute_mandelbrot.py
@@ -4,7 +4,7 @@ from matplotlib.pylab import imshow, show, savefig
 def compute_mandelbrot(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=1000, plot_filename=None, mandelbrot_class=MandelbrotNumpy):
     mc = mandelbrot_class(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=max_escape_time)
     escape_matrix = mc()
-    imshow(escape_matrix)
     if plot_filename:
+        imshow(escape_matrix)
         savefig(plot_filename)
-    show()
+    return escape_matrix
