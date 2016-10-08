6b2e46e2a02b2d0b0f189046c88707a541bf0051
Schoyen-Linux <oyvinssc@student.matnat.uio.no>
2016-10-08 12:51:58 +0200

    Start implementation of compute_mandelbrot

---
+++ assignment4/mandelbrot/mandelbrot/compute_mandelbrot.py
@@ -0,0 +1,9 @@
+from . import MandelbrotNumpy
+from matplotlib.pylab import imshow, show, savefig
+
+def compute_mandelbrot(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=1000, plot_filename=None, mandelbrot_class=MandelbrotNumpy):
+    mc = mandelbrot_class(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=max_escape_time)
+    escape_matrix = mc()
+    imshow(escape_matrix)
+    savefig(plot_filename)
+    show()
