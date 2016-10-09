22aac7a44b9185867cd9ef3d126834f969cb9263
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-09 14:34:46 +0200

    Update mandelbrot_3

--- assignment4/mandelbrot/mandelbrot/src/mandelbrot_3.py
+++ assignment4/mandelbrot/mandelbrot/src/mandelbrot_3.py
@@ -1,5 +1,4 @@
 from .mandelbrot_cython import mandelbrot_set_cython
-from time import time
 
 class MandelbrotCython:
 
@@ -7,5 +6,4 @@ class MandelbrotCython:
         self.args = [xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time, divergence_criteria]
 
     def __call__(self):
-        m_set = mandelbrot_set_cython(*self.args)
-        return m_set
+        return mandelbrot_set_cython(*self.args)
