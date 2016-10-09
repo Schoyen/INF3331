64070bad77a9adfa3a644c2362729467cd973434
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-09 14:49:46 +0200

    Update profile_mandelbrot

--- assignment4/profile_mandelbrot.py
+++ assignment4/profile_mandelbrot.py
@@ -1,22 +1,20 @@
 from mandelbrot import MandelbrotPython, MandelbrotNumpy, MandelbrotCython
-from cProfile import Profile
-from pstats import Stats
 from matplotlib.pylab import imshow, show
+from time import time
 
 function_map = [MandelbrotPython, MandelbrotNumpy, MandelbrotCython]
 args = [-3, 3, -3, 3, 1000, 1000] # [xmin, xmax, ymin, ymax, Nx, Ny]
 max_iterations = 1000
-mp = MandelbrotPython(*args, max_escape_time=max_iterations)
+divergence = 2
+time_list = []
 
-profile = Profile()
-profile.enable()
-#profile.run('mp()')
-mat = mp()
-profile.disable()
-profile.dump_stats('test.prof')
-stats = Stats('test.prof')
-stats.sort_stats('time')
-stats.print_stats(3)
+for func in function_map:
+    mp = func(*args, max_escape_time=max_iterations)
+    t0 = time()
+    mat = mp()
+    t1 = time()
+    time_list.append(t1 - t0)
+    imshow(mat)
+    show()
 
-imshow(mat)
-show()
+print (time_list)
