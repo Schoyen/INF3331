bbb9759c8d3cb32a1d029eb643e83a693c8a9b3e
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-09 19:41:59 +0200

    Add test of region in compute_mandelbrot

--- assignment4/mandelbrot/mandelbrot/compute_mandelbrot.py
+++ assignment4/mandelbrot/mandelbrot/compute_mandelbrot.py
@@ -1,5 +1,6 @@
 from . import MandelbrotCython # Include a "standard" (the fastest ZOMG!)
 from matplotlib.pylab import imshow, savefig
+from numpy import all as npall
 
 def compute_mandelbrot(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=1000, plot_filename=None,
         mandelbrot_class=MandelbrotCython, divergence_criteria=2):
@@ -18,8 +19,36 @@ def compute_mandelbrot(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=1000, plo
         mandelbrot_class:       The desired class for computing the Mandelbrot set.
     """
     mc = mandelbrot_class(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=max_escape_time, divergence_criteria=divergence_criteria)
-    escape_matrix = mc() # Calculate the Mandelbrot set
+    escape_matrix, ok = check_area(xmin, xmax, ymin, ymax, Nx, Ny, divergence_criteria)
+    if ok:
+        escape_matrix = mc() # Calculate the Mandelbrot set
     if plot_filename: # If the user specified a filename
         imshow(escape_matrix)
         savefig(plot_filename)
     return escape_matrix
+
+def check_area(xmin, xmax, ymin, ymax, Nx, Ny, divergence_criteria):
+    """Function used for testing if the region is sensible.
+
+    Args:
+        xmin:                   Start coordinate for the real axis.
+        xmax:                   Stop coordinate for the real axis.
+        ymin:                   Start coordinate for the complex axis.
+        ymax:                   Stop coordinate for the complex axis.
+        Nx:                     The number of steps along the real axis.
+        Ny:                     The number of steps along the complex axis.
+        divergence_criteria:    A float used to determine if a point in the complex plane has diverged.
+    """
+    test_outside_escape = 10
+    tol = 1e-12
+    mc = MandelbrotCython(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=test_outside_escape, divergence_criteria=divergence_criteria)
+    mat = mc()
+    ok = True
+    if npall((mat - 1.0) < tol):
+        print ("Warning: The specified region [%g, %g] x [%g, %g] is completely outside the Mandelbrot set" % (xmin, xmax, ymin, ymax))
+        ok = False
+    elif npall((mat - test_outside_escape) < tol):
+        print ("Warning: The specified region [%g, %g] x [%g, %g] is completely inside the Mandelbrot set" % (xmin, xmax, ymin, ymax))
+        ok = False
+
+    return mat, ok
