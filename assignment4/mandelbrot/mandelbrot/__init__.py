28f9f18b70305cb4d539313a71268ebd45708ce4
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-09 18:07:11 +0200

    Comment __init__.py

--- assignment4/mandelbrot/mandelbrot/__init__.py
+++ assignment4/mandelbrot/mandelbrot/__init__.py
@@ -1,2 +1,3 @@
 from mandelbrot.src import MandelbrotPython, MandelbrotNumpy, MandelbrotCython, mandelbrot_set_cython
 from mandelbrot.compute_mandelbrot import compute_mandelbrot
+# Script handling all imports needed for the module
--- assignment4/mandelbrot/mandelbrot/src/__init__.py
+++ assignment4/mandelbrot/mandelbrot/src/__init__.py
@@ -2,3 +2,4 @@ from .mandelbrot_1 import MandelbrotPython
 from .mandelbrot_2 import MandelbrotNumpy
 from .mandelbrot_3 import MandelbrotCython
 from .mandelbrot_cython import mandelbrot_set_cython
+# Script handling imports between dirs in module
