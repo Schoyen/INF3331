71c57636402ef20cf803018b2868317c4606f33f
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-09 18:07:42 +0200

    Remove old mandelbrot-client

--- assignment4/mandelbrot-client/src/commands/mandelbrot_3.py
+++
@@ -1,9 +0,0 @@
-from mandelbrot import mandelbrot_set
-from matplotlib.pylab import imshow, show
-
-height = 100
-width = 100
-threshold = 100
-divergence_criteria = 2
-imshow(mandelbrot_set(height, width, threshold, divergence_criteria))
-show()
