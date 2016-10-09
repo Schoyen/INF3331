6d65772331dc4df4fba41ed9428d12dbde730e6d
oyvinssc <oyvinssc@student.matnat.uio.no>
2016-10-09 11:41:37 +0200

    Remove time in compute_mandelbrot.py

--- assignment4/mandelbrot/mandelbrot/compute_mandelbrot.py
+++ assignment4/mandelbrot/mandelbrot/compute_mandelbrot.py
@@ -1,16 +1,11 @@
 from . import MandelbrotNumpy
 from matplotlib.pylab import imshow, savefig
-from time import time
 
 def compute_mandelbrot(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=1000, plot_filename=None,
         mandelbrot_class=MandelbrotNumpy, divergence_criteria=2):
     mc = mandelbrot_class(xmin, xmax, ymin, ymax, Nx, Ny, max_escape_time=max_escape_time, divergence_criteria=divergence_criteria)
-    t0 = time()
     escape_matrix = mc()
-    t1 = time()
-    print ("{0}: {1:.4} s".format(mc.__class__.__name__, t1 - t0))
     if plot_filename:
         imshow(escape_matrix)
         savefig(plot_filename)
-    print (escape_matrix)
     return escape_matrix
