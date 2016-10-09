1d71a536ad7a3f8a34cb8dfa28e8e1c9d860873c
Schoyen <oyvindschoyen@gmail.com>
2016-10-09 09:02:04 +0200

    Comment self replication program

--- assignment4/self_replication.py
+++ assignment4/self_replication.py
@@ -1,2 +1,2 @@
-var = 'var = %r\nprint(var %% var)'
-print (var % var)
+var = 'var = %r # Using %%r for repr yielding the raw string\nprint(var %% var) # Printing var with var as an argument to %%r' # Using %r for repr yielding the raw string
+print (var % var) # Printing var with var as an argument to %r
