21d44e56b980472c1f2897600d246488e2dc5b7c
Schoyen <oyvindschoyen@gmail.com>
2016-10-23 17:41:27 +0200

    Add start for highlighter.py

---
+++ assignment5/highlighter.py
@@ -0,0 +1,41 @@
+from sys import argv
+from re import findall, sub, search
+
+def highlighter(regex, color_theme, source_code):
+    for key in regex.keys():
+        if search(r"%s" % regex[key], source_code):
+            replacement_string = "\033[{0}m{1}\033[0m".format(color_theme[key], ''.join(findall("%s" % regex[key], source_code)))
+            new_source = sub(r"%s" % regex[key], replacement_string, source_code)
+            print (new_source)
+
+def read_syntax(syntaxfile):
+    regex_dict = {}
+    with open(syntaxfile, 'r') as f:
+        for line in f:
+            regex_dict[findall(r": (\w+$)", line)[0]] = findall(r"\"(.*)\":", line)[0]
+    return regex_dict
+
+def read_theme(themefile):
+    theme_dict = {}
+    with open(themefile, 'r') as f:
+        for line in f:
+            theme_dict[findall(r"(^\w*):", line)[0]] = findall(r": (.*$)", line)[0]
+    return theme_dict
+
+def read_source(sourcefile):
+    with open(sourcefile, 'r') as f:
+        return f.read()
+
+if __name__ == '__main__':
+    try:
+        syntaxfile = argv[1]
+        themefile = argv[2]
+        sourcefile = argv[3]
+    except IndexError:
+        raise IndexError("""Not enough command-line arguments.
+Usage: %s <syntaxfile> <themefile> <sourcefile>""" % argv[0])
+
+    regex = read_syntax(syntaxfile)
+    color_theme = read_theme(themefile)
+    source_code = read_source(sourcefile)
+    highlighter(regex, color_theme, source_code)
