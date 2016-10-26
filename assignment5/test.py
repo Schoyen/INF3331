56f2d00c0286089deabb1afb9491f33864568ccb
Mint-kontor <oyvindschoyen@gmail.com>
2016-10-26 10:53:36 +0200

    Add working class coloring

--- assignment5/test.py
+++ assignment5/test.py
@@ -1,4 +1,30 @@
 
+class MyObject(object):
+
+    def __init__(self, obviously):
+        self.cat = obviously
+
+    def __call__(self):
+        pass
+
+class SubObject(MyObject):
+
+    def __init__(self, obviously):
+        self.__init__(obviously)
+
+    def __call__(self):
+        pass
+
+    class InternalClass(object):
+
+        def __init__(self):
+            pass
+
+        def somefunc(self):
+            pass
+
+        def 1_lol(self):
+            pass
 
 def foo():
     print ("YOLO")
@@ -8,6 +34,12 @@ def defedef():
 
     def defidef():
         print ("HAHHA")
+
+        def deffelef():
+            pass
+
+            def deffind():
+                pass
 
 # def haha()
  # def lol()
