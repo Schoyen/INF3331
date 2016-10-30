8f7b5ad6b1352db5c6b2b8386aaf36ca3dfd6cf5
Schoyen <oyvindschoyen@gmail.com>
2016-10-30 19:04:33 +0100

    Fix some whitespace handling
        Several of the regexes have trouble highlighting only the actual
        (desired) match. Checking for comment with look-behind is also
        non-trivial.

--- assignment5/test.py
+++ assignment5/test.py
@@ -18,17 +18,27 @@ class MyObject(object):
     def __call__(self):
         pass
 
+#"hello"
+# "halla"
 @public
 @
 @ test
 # @test
+# b_tull = 1000
+# b = 10
 class SubObject(MyObject):
     @x.get
     def __init__(self, obviously):
         self.__init__(obviously)
 
     def __call__(self):
-        pass
+        a = 10
+        _c = 100
+        a_10 = 10
+        # a = 0
+        #a = 10
+        #b=1
+        c=20
 
     class InternalClass(object):
 
@@ -88,3 +98,8 @@ def _a():
     pass # def test()
 
 _b()
+
+try:
+    print (a)
+except IndexError:
+    pass
