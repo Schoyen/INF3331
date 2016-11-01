06704906940b04d7a9ff68a63882410fc1a23b31
Mint-kontor <oyvindschoyen@gmail.com>
2016-11-01 15:13:21 +0100

    Add naive start of my_diff

--- assignment5/superdiff/my_diff.py
+++ assignment5/superdiff/my_diff.py
@@ -10,7 +10,26 @@ class Superdiff:
     def __call__(self):
         original = self._read_from_file(original_file)
         modified = self._read_from_file(modified_file)
+        for i in range(len(original)):
+            if i < len(modified):
+                if original[i] == modified[i]:
+                    print ("0 %s" % original[i])
 
     def _read_from_file(self, filename):
         with open(filename, 'r') as f:
-            return f.read()
+            return [line for line in f]
+
+if __name__ == '__main__':
+    from sys import argv
+    try:
+        original_file = argv[1]
+        modified_file = argv[2]
+    except IndexError:
+        raise IndexError("Usage: %s <original file> <modified file> <output file> (optional)" % argv[0])
+
+    try:
+        sd = Superdiff(original_file, modified_file, argv[3])
+    except IndexError:
+        sd = Superdiff(original_file, modified_file)
+
+    sd()
