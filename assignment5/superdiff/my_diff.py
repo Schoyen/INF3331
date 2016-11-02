ea9ca7992e57f00cd8bf3d33abd539e6d56256cc
Mint-kontor <oyvindschoyen@gmail.com>
2016-11-02 16:52:22 +0100

    Add tests for lines
            This commit adds a messy way of iterating through the lines.

--- assignment5/superdiff/my_diff.py
+++ assignment5/superdiff/my_diff.py
@@ -10,14 +10,34 @@ class Superdiff:
     def __call__(self):
         original = self._read_from_file(original_file)
         modified = self._read_from_file(modified_file)
+        self.tagged = {}
+
         for i in range(len(original)):
             if i < len(modified):
                 if original[i] == modified[i]:
-                    print ("0 %s" % original[i])
+                    self.tagged[i] = modified[i]
+                    print("0 %s" % original[i])
+                else:
+                    if original[i] in modified:
+                        self._find_match(original[i], modified, i)
+                    else:
+                        print("- %s" % original[i])
+            else:
+                print("- %s" % original[i])
+
+        for i in range(len(modified)):
+            if i < len(original):
+                if i not in sorted(self.tagged):
+                    pass
 
     def _read_from_file(self, filename):
         with open(filename, 'r') as f:
-            return [line for line in f]
+            return [line.strip() for line in f]
+
+    def _find_match(word, modified, position):
+        for i in range(len(modified)):
+            if word == modified[i] and position < i:
+                print("0 %s" % modified)
 
 if __name__ == '__main__':
     from sys import argv
