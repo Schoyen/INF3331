c652d126c68c085f1c06306cc06e8f1a92383f2f
Schoyen <oyvindschoyen@gmail.com>
2016-11-06 14:31:54 +0100

    Document highlighter-method

--- assignment5/highlighter.py
+++ assignment5/highlighter.py
@@ -1,42 +1,67 @@
-from sys import argv
+from sys import argv, exit
 from re import findall, sub, MULTILINE, DOTALL
 
 def highlighter(regex, color_theme, source_code):
+    """Function used to replace a regex with itself but with bash-coloring wrapped around.
+    Args:
+        regex: A regex-pattern.
+        color_theme: An ANSI escape code to color the matched regex.
+        source_code: The complete source code to search for a regex match.
+
+    Returns:
+        str: The (possibly) updated source code string with color formatting.
+    """
     return sub(regex, lambda hit: "\033[{0}m{1}\033[0m".format(color_theme, hit.group()), source_code, flags=MULTILINE|DOTALL)
 
-def _read_syntax(syntaxfile):
-    regex_dict = {}
-    with open(syntaxfile, 'r') as f:
-        for line in f:
-            regex_dict[findall(r": (\w+$)", line)[0]] = findall(r"\"(.*)\":", line)[0]
-    return regex_dict
+def _read_file(filename, key, value):
+    """Function used to create a dictionary from the syntax and theme files for highlighting.
 
-def _read_theme(themefile):
-    theme_dict = {}
-    with open(themefile, 'r') as f:
+    Args:
+        filename: The filename of a syntax/theme-file.
+        key: A regex with the pattern to search for in the file to use as key in the dictionary.
+        value: A regex with the pattern to search for in the file to use as a value in the dictionary.
+
+    Returns:
+        dict: A dictionary where the key is the name of the pattern to search for in highlighter and
+              where the value is either a regex or an ANSI escape code.
+    """
+    ret_dict = {}
+    with open(filename, 'r') as f:
         for line in f:
-            theme_dict[findall(r"(^\w*):", line)[0]] = findall(r": (.*$)", line)[0]
-    return theme_dict
+            ret_dict[findall(key, line)[0]] = findall(value, line)[0]
+    return ret_dict
 
 def _read_source(sourcefile):
+    """Function used to read the entire source code verbatim.
+
+    Args:
+        sourcefile: The filename of the source code.
+
+    Returns:
+        str: The source code.
+    """
     with open(sourcefile, 'r') as f:
         return f.read()
 
-def _sort_dictonary(dictionary):
-    """Create a list with sorted keys to make sure coloring is done in the right order.
-
-    This function filters on *_n where n is a number from 1 and rising. The number only
-    counts for regexes with the same name and differing number. A *_0 means that this
-    regex will occur last. If there are no numbers this regex pattern will occur randomly.
-    """
-    sorted_list = []
-    for key in dictionary:
-        sorted_list.append(key)
-    return sorted(sorted_list)
-
 def highlight_source(regex_dict, color_theme_dict, source_code):
-    sorted_keys = _sort_dictonary(regex_dict)
-    for key in sorted_keys:
+    """Function used to iterate over all the keys and values in the regex and color theme dictionaries.
+
+    The function proceeds to call the highlighter method with a regex and a theme with a gradually more
+    colored source code.
+
+    Args:
+        regex_dict: A dictionary with the name of the pattern as a key and the regex pattern as value.
+        color_theme_dict: A dictionary with the name of the pattern as a key and the color theme as value.
+        source_code: The entire source code as a string.
+
+    Returns:
+        str: The (possibly) colored source code.
+    """
+
+    msg = """The keys in regex_dict and color_theme dict are unequal.
+Make sure the .syntax and .theme file share the same name on the regex and ANSI escape code."""
+    assert sorted(regex_dict) == sorted(color_theme_dict), msg
+    for key in sorted(regex_dict):
         source_code = highlighter(regex_dict[key], color_theme_dict[key], source_code)
     return source_code
 
@@ -46,17 +71,12 @@ if __name__ == '__main__':
         themefile = argv[2]
         sourcefile = argv[3]
     except IndexError:
-        raise IndexError("""Not enough command-line arguments.
+        print("""Not enough command-line arguments.
 Usage: %s <syntaxfile> <themefile> <sourcefile>""" % argv[0])
+        exit(1) # EXIT_FAILURE
 
-    regex = _read_syntax(syntaxfile)
-    color_theme = _read_theme(themefile)
+    regex = _read_file(syntaxfile, r": (\w+$)", r"\"(.*)\":")
+    color_theme = _read_file(themefile, r"(^\w*):", r": (.*$)")
     source_code = _read_source(sourcefile)
 
     print (highlight_source(regex, color_theme, source_code))
-
-#    syntax_text = {}
-#    for key in regex:
-#        syntax_text[key] = highlighter(regex[key], color_theme[key], source_code)
-#    print (syntax_text['def_function'])
-#    print (syntax_text['function_text'])
