79324f6c9d59f34d11dacf6927f558a62f112364
Schoyen <oyvindschoyen@gmail.com>
2016-11-06 14:10:04 +0100

    Add syntax highlighting for java and test.java
        There is currently an issue where the negative lookbehind does not
        catch a comment.

---
+++ assignment5/test.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+// class Commented
+// {
+//     public int foo(int a, int b)
+//     {
+//         return a == b;
+//     }
+// }
+
+class Awesome
+{
+    private bar(char a, char c)
+    {
+        return c != a;
+    }
+}
+/**An object used to start it all.
+ *
+ * Bleh
+ */
+class Main
+{
+    public static void main(String []args)
+    {
+        System.out.println("HUZZAH!");
+    }
+}
