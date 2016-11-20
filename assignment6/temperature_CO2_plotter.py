dde6ace21fe1bf5133ade8e042dcb269d89732b5
Schoyen <oyvindschoyen@gmail.com>
2016-11-20 16:29:30 +0100

    Add bar chart plots for country emission
            This is not yet fully integrated in the class.

--- assignment6/temperature_CO2_plotter.py
+++ assignment6/temperature_CO2_plotter.py
@@ -1,5 +1,6 @@
 from pandas import read_csv
-from matplotlib.pylab import plot, show, axis, savefig, xlabel, ylabel, title
+from matplotlib.pylab import plot, show, axis, savefig, xlabel, ylabel, title, bar, xticks, axhline, legend
+from numpy import zeros, arange
 
 
 class PlotClimateData:
@@ -23,9 +24,19 @@ class PlotClimateData:
         plot(co2_data["Year"], co2_data["Carbon"])
         self._set_plot_info("Year", "Cabon [Gkg]","Cabon emissions in giga kilograms per year")
 
-    def plot_CO2_emissions_per_country(self, upper_threshold=None, lower_threshold=None):
+    def plot_CO2_emissions_per_country(self, lower_threshold=None, upper_threshold=None, year=2013):
         co2_country_data = read_csv("dat/CO2_by_country.csv")
-        print (co2_country_data)
+        emission_data = co2_country_data[str(year)]
+        upper_threshold = upper_threshold or max(emission_data.values) + 1e-10 # Add a small tolerance due to float-precision
+        lower_threshold = lower_threshold or 0
+        indices = (emission_data.values <= upper_threshold) & (emission_data.values >= lower_threshold)
+        x_indices = arange(len(co2_country_data["Country Code"].values[indices]))
+        bar(x_indices, emission_data.values[indices], align='center')
+        xticks(x_indices, co2_country_data["Country Code"].values[indices], rotation='vertical')
+        axhline(lower_threshold, linewidth=2, color='g', label="Lower threshold")
+        axhline(upper_threshold, linewidth=2, color='r', label="Upper threshold")
+        legend(loc='best')
+        show()
 
     def _set_plot_info(self, XLABEL, YLABEL, TITLE):
         axis([self.tmin, self.tmax, self.ymin, self.ymax])
@@ -41,4 +52,4 @@ if __name__ == '__main__':
     pcd = PlotClimateData()
     #pcd.plot_temperature()
     #pcd.plot_CO2()
-    pcd.plot_CO2_emissions_per_country()
+    pcd.plot_CO2_emissions_per_country(upper_threshold=10, lower_threshold=8)
