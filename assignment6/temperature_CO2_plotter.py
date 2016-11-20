3d04278f05759b852355abb07c7283f59b2e6acb
Schoyen <oyvindschoyen@gmail.com>
2016-11-20 14:49:47 +0100

    Update the functions to class functions
        This allows more reuse of the plotting functionality and the
        parameters.

--- assignment6/temperature_CO2_plotter.py
+++ assignment6/temperature_CO2_plotter.py
@@ -1,28 +1,44 @@
 from pandas import read_csv
 from matplotlib.pylab import plot, show, axis, savefig, xlabel, ylabel, title
 
-def plot_temperature(month="January", tmin=None, tmax=None, ymin=None, ymax=None, SAVEFIG=None):
-    temperature_data = read_csv("dat/temperature.csv")
-    plot(temperature_data["Year"], temperature_data[month])
-    axis([tmin, tmax, ymin, ymax])
-    xlabel("Year")
-    ylabel("Temperature [Celsius]")
-    title("Average tempature for %s per year" % month)
-    if SAVEFIG:
-        savefig(SAVEFIG)
-    show()
 
-def plot_CO2(tmin=None, tmax=None, ymin=None, ymax=None, SAVEFIG=None):
-    co2_data = read_csv("dat/co2.csv")
-    plot(co2_data["Year"], co2_data["Carbon"])
-    axis([tmin, tmax, ymin, ymax])
-    xlabel("Year")
-    ylabel("Carbon [Gkg]")
-    title("Cabon emissions in giga kilograms per year")
-    if SAVEFIG:
-        savefig(SAVEFIG)
-    show()
+class PlotClimateData:
+
+    def __init__(self, tmin=None, tmax=None, ymin=None, ymax=None, SAVEFIG=None, show_image=True):
+        self.tmin = tmin
+        self.tmax = tmax
+        self.ymin = ymax
+        self.ymax = ymax
+        self.SAVEFIG = SAVEFIG
+        self.show_image = show_image
+
+    def plot_temperature(self, month="January"):
+        temperature_data = read_csv("dat/temperature.csv")
+        plot(temperature_data["Year"], temperature_data[month])
+        self._set_plot_info("Year", "Temperature [Celsius]", "Average tempature for %s per year" % month)
+
+
+    def plot_CO2(self):
+        co2_data = read_csv("dat/co2.csv")
+        plot(co2_data["Year"], co2_data["Carbon"])
+        self._set_plot_info("Year", "Cabon [Gkg]","Cabon emissions in giga kilograms per year")
+
+    def plot_CO2_emissions_per_country(self, upper_threshold=None, lower_threshold=None):
+        co2_country_data = read_csv("dat/CO2_by_country.csv")
+        print (co2_country_data)
+
+    def _set_plot_info(self, XLABEL, YLABEL, TITLE):
+        axis([self.tmin, self.tmax, self.ymin, self.ymax])
+        xlabel(XLABEL)
+        ylabel(YLABEL)
+        title(TITLE)
+        if self.SAVEFIG:
+            savefig(self.SAVEFIG)
+        if self.show_image:
+            show()
 
 if __name__ == '__main__':
-    plot_temperature()
-    plot_CO2()
+    pcd = PlotClimateData()
+    #pcd.plot_temperature()
+    #pcd.plot_CO2()
+    pcd.plot_CO2_emissions_per_country()
