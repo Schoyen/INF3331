995034d3e0009116bb7d3e9706ea32373b8faa1f
Schoyen <oyvindschoyen@gmail.com>
2016-11-24 17:32:11 +0100

    Add visualization of temperature plot

--- assignment6/web_visualization.py
+++ assignment6/web_visualization.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from base64 import b64encode
 from flask import Flask, render_template
-from temperature_CO2_plotter import plot_CO2
+from temperature_CO2_plotter import plot_CO2, plot_temperature
 web_app = Flask(__name__)
 
 @web_app.route("/")
@@ -15,6 +15,14 @@ def visualize_co2_data():
     image_file.seek(0)
     image = b64encode(image_file.getvalue())
     return render_template("co2_data.html", image=image)
+
+@web_app.route("/temperature/")
+def visualize_temperature_data():
+    image_file = BytesIO()
+    plot_temperature(show_image=False, SAVEFIG=image_file)
+    image_file.seek(0)
+    image = b64encode(image_file.getvalue())
+    return render_template("temperature.html", image=image)
 
 if __name__ == '__main__':
     web_app.run(debug=True)
