e4250e5f941f19585a6d040eb18f53bbcd448f77
Schoyen <oyvindschoyen@gmail.com>
2016-11-24 17:36:55 +0100

    Add CO2-plot for countries on the web page

--- assignment6/web_visualization.py
+++ assignment6/web_visualization.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from base64 import b64encode
 from flask import Flask, render_template
-from temperature_CO2_plotter import plot_CO2, plot_temperature
+from temperature_CO2_plotter import plot_CO2, plot_temperature, plot_CO2_emissions_per_country
 web_app = Flask(__name__)
 
 @web_app.route("/")
@@ -23,6 +23,14 @@ def visualize_temperature_data():
     image_file.seek(0)
     image = b64encode(image_file.getvalue())
     return render_template("temperature.html", image=image)
+
+@web_app.route("/co2_by_country/")
+def visualize_country_co2():
+    image_file = BytesIO()
+    plot_CO2_emissions_per_country(show_image=False, SAVEFIG=image_file)
+    image_file.seek(0)
+    image = b64encode(image_file.getvalue())
+    return render_template("country_co2.html", image=image)
 
 if __name__ == '__main__':
     web_app.run(debug=True)
