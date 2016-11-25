de897eec8359e409a16d525927a3eb8686da2286
Schoyen <oyvindschoyen@gmail.com>
2016-11-26 00:05:19 +0100

    Add interactive visualization for co2_data.
        This commit includes POST-handling in the Flask-app for co2_data as
        well as error handling and input-forms in the HTML-file for
        co2_data.

--- assignment6/web_visualization.py
+++ assignment6/web_visualization.py
@@ -1,7 +1,8 @@
 from io import BytesIO
 from base64 import b64encode
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from temperature_CO2_plotter import plot_CO2, plot_temperature, plot_CO2_emissions_per_country
+from matplotlib.pylab import clf
 web_app = Flask(__name__)
 
 @web_app.route("/")
@@ -9,12 +10,40 @@ def front_page():
     return render_template("index.html")
 
 @web_app.route("/co2_data/")
-def visualize_co2_data():
+def visualize__initial_co2_data():
     image_file = BytesIO()
     plot_CO2(show_image=False, SAVEFIG=image_file)
     image_file.seek(0)
     image = b64encode(image_file.getvalue())
     return render_template("co2_data.html", image=image)
+
+@web_app.route("/co2_data/handle_input_co2_data", methods=["Post"])
+def visualize_co2_data():
+    assert request.method == "POST" # Test
+    clf() # Clear figure to avoid plotting on top of old image
+    image = None
+    error_message = []
+    error = False
+
+    tmin = int(request.form["tmin"])
+    tmax = int(request.form["tmax"])
+    ymin = int(request.form["ymin"])
+    ymax = int(request.form["ymax"])
+
+    if ymax <= ymin:
+        error_message.append("The maximum carbon ({0}) emission must be greater than the minimum ({1}).".format(ymax, ymin))
+        error = True
+
+    if tmax <= tmin:
+        error_message.append("The final year ({0}) must be greater than the initial year ({1}).".format(tmax, tmin))
+        error = True
+
+    if not error:
+        image_file = BytesIO()
+        plot_CO2(tmin=tmin, tmax=tmax, ymin=ymin, ymax=ymax, show_image=False, SAVEFIG=image_file)
+        image_file.seek(0)
+        image = b64encode(image_file.getvalue())
+    return render_template("co2_data.html", image=image, error=error, error_message=error_message)
 
 @web_app.route("/temperature/")
 def visualize_temperature_data():
